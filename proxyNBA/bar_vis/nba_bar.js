a714ac6a93109189e92423faf210facb2d52ba9e
pjsalisbury <patrickjsalisbury@gmail.com>
2024-02-15 23:53:24 -0500

    bar viz

---
+++ proxyNBA/bar_vis/nba_bar.js
@@ -0,0 +1,119 @@
+let playerIDs = [];
+let playerPPGs = [];
+
+const margin = {top: 20, right: 20, bottom: 20, left: 20};
+const legendSize = 100;
+const plotWidth = window.innerWidth * 0.8 - margin.left - margin.right - legendSize;
+const plotHeight = 600 - margin.top - margin.bottom;
+
+fetch('/api/playerindex?LeagueID=00&Season=2023-24')
+    .then(response => response.json())
+    .then(res => {
+        let players = [];
+        res['PlayerIndex'].forEach( e => {
+            players.push(e);
+        });
+        playerIDs = getRandomPlayerIDs(players, 1);
+        console.log('Randomly selected player IDs:', playerIDs);
+        
+        // Ensure all player data gets fetched first
+        Promise.all(playerIDs.map(playerID =>
+            fetch(`/api/playergamelog?DateFrom=&DateTo=&LeagueID=&PlayerID=${playerID}&Season=2023-24&SeasonType=Regular+Season`)
+                .then(response => response.json())
+                .then(data => data['PlayerGameLog'])
+        ))
+        .then(playerGameLogs => {
+            // Calculate PPG
+            playerPPGs = playerGameLogs.map(playerGameLog => {
+                const totalPoints = playerGameLog.reduce((sum, game) => sum + parseInt(game['PTS']), 0);
+                const totalGames = playerGameLog.length;
+                const ppg = totalPoints / totalGames;
+                return ppg;
+            });
+
+            console.log('Player PPGs:', playerPPGs);
+
+            // Merge player data
+            const playersCombined = playerGameLogs.flatMap((playerGameLog, index) => {
+                const playerID = playerIDs[index];
+                return playerGameLog.map(game => ({
+                    playerID,
+                    date: new Date(game['GAME_DATE']),
+                    points: game['PTS']
+                }));
+            });
+
+            // barchart
+            const svg = d3.select('#barchart-container').append('svg')
+                .attr('width', plotWidth + margin.left + margin.right + legendSize)
+                .attr('height', plotHeight + margin.top + margin.bottom)
+                .append('g')
+                .attr('transform', `translate(${margin.left},${margin.top})`);
+
+            const xScale = d3.scaleTime()
+                .domain(d3.extent(playersCombined, d => d.date))
+                .range([0, plotWidth]);
+
+            const yScale = d3.scaleLinear()
+                .domain([-0.2, d3.max(playersCombined, d => d.points)])
+                .range([plotHeight, 0]);
+
+            svg.append('g')
+                .attr('transform', `translate(0,${plotHeight})`)
+                .call(d3.axisBottom(xScale));
+
+            svg.append('g')
+                .call(d3.axisLeft(yScale));
+
+            // bars
+            svg.selectAll('mybar')
+                .data(playersCombined)
+                .enter()
+                .append("rect")
+                    .attr("x", function(d) { return xScale(d.date); })
+                    .attr("y", function(d) { return yScale(d.points); })
+                    .attr("width", 10)
+                    .attr("height", function(d) { return plotHeight - yScale(d.points); })
+                    .attr("fill", function(d) { 
+                        if(d.points == 0) {
+                            return 'red';
+                        } else {
+                            return 'green';  
+                        }})
+
+            // top right next to bar chart
+            const legend = svg.append('g')
+                .attr('transform', `translate(${plotWidth + margin.right},${margin.top})`);
+
+            playerIDs.forEach((playerID, i) => {
+                legend.append('rect')
+                    .attr('x', 0)
+                    .attr('y', i * 20)
+                    .attr('width', 10)
+                    .attr('height', 10)
+                    .attr('fill', 'green');
+
+                legend.append('text')
+                    .attr('x', 15)
+                    .attr('y', i * 20 + 10)
+                    .attr('dy', '0.35em')
+                    .text(playerID);
+            });
+        })
+        .catch(error => console.error('playerGameLog error:', error));
+    })
+    .catch(error => console.error('playerIndex error:', error));
+
+// Helper function to pick X random NBA players
+function getRandomPlayerIDs(data, count) {
+    const randomPlayerIDs = [];
+    let personIds = [];
+    for (let i = 0; i < data.length; i++) {
+        personIds.push(data[i]['PERSON_ID'])
+    }
+    for (let i = 0; i < count; i++) {
+        const randomIndex = Math.floor(Math.random() * personIds.length);
+        randomPlayerIDs.push(personIds[randomIndex]);
+    }
+    return randomPlayerIDs;
+}
