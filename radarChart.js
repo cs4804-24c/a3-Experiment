519d362f81ddd97c1e15677c2e50afd93aeeab69
kylieflerlage <128189858+kylieflerlage@users.noreply.github.com>
2024-02-19 15:55:46 -0500

    Update radarChart.js

--- radarChart.js
+++ radarChart.js
@@ -22,7 +22,35 @@ function RadarChart(id, data, options) {
 	 hasFill: false,
 	 color: ["red", "green","blue"]	//Color function
 	};
-	
+
+	function randomPoints(){
+		data = [
+        [
+        //employee 1
+            {axis:"Creativity", value: Math.floor(Math.random()*100)},
+            {axis:"Leadership", value: Math.floor(Math.random()*100)},
+            {axis:"Communication", value: Math.floor(Math.random()*100)},
+            {axis:"Time Management", value: Math.floor(Math.random()*100)},
+            {axis:"Teamwork",value:  Math.floor(Math.random()*100)},
+        ],
+        [
+        //employee 2
+            {axis:"Creativity", value: Math.floor(Math.random()*100)},
+            {axis:"Leadership", value: Math.floor(Math.random()*100)},
+            {axis:"Communication", value: Math.floor(Math.random()*100)},
+            {axis:"Time Management", value: Math.floor(Math.random()*100)},
+            {axis:"Teamwork",value: Math.floor(Math.random()*100)},
+        ],
+        [
+        //employee 3
+            {axis:"Creativity", value: Math.floor(Math.random()*100)},
+            {axis:"Leadership", value: Math.floor(Math.random()*100)},
+            {axis:"Communication", value: Math.floor(Math.random()*100)},
+            {axis:"Time Management", value: Math.floor(Math.random()*100)},
+            {axis:"Teamwork",value:  Math.floor(Math.random()*100)},
+        ] 
+    ];
+	}
 	//Put all of the options into a variable called cfg
 	if('undefined' !== typeof options){
 	  for(var i in options){
